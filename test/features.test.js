4b1d76b98132dce34ea88a333bbc59d366a8a5cb
mkelleyharris <kelley@mkharris.com>
2022-04-25 11:49:26 -0700

    rename src to js dir

--- test/features.test.js
+++ test/features.test.js
@@ -4,7 +4,7 @@
 // usage: deno test  // from parent project's root dir
 
 import {assertEquals} from "https://deno.land/std@0.126.0/testing/asserts.ts";
-import {Features} from '../src/features.js';
+import {Features} from '../js/features.js';
 
 Deno.test("On create, no features", () => {
   const features = new Features();
